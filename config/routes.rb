8a944a6a98109b3828875bac5e7e11800fb8a0fb
Ezra Shim <ezrashim@gmail.com>
2017-02-08 00:36:39 -0500

    fun

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Davidshim::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+    root 'main#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,7 +13,7 @@ Davidshim::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :main, only: [:index]
   # Example resource route with options:
   #   resources :products do
   #     member do
